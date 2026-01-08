0ceaa850c4d5101f7515c925446f146345251e2c
AlexUrluescuWork <urluescualex2003@gmail.com>
2026-01-08 12:16:00 +0200

    abstract AI class

---
+++ my-flask-app/ai_models_connection/main.py
@@ -0,0 +1,10 @@
+from ai_models_connection.ai_provider import BaseLLMProvider
+
+class InitModelAI:
+    def __init__(self, provider: BaseLLMProvider):
+        self.llm = provider.get_llm()
+        self.analysis_chain = None
+
+    def set_analysis_chain(self, prompt_chain):
+        """Combine prompt with the LLM to create a full chain."""
+        self.analysis_chain = prompt_chain | self.llm
